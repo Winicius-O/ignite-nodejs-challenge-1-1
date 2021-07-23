c11795d8e45f513990abf64607c4dda67f07b1bb
winicius-o <wini@DESKTOP-N3NJ0NA.localdomain>
2021-07-23 04:17:44 -0300

    username middleware

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username == username);
+  if(!user){
+    return response.status(400).json({error: "Username not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 /* 
@@ -37,7 +46,10 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  //testing middleware
+  const {user} = request;
+
+  return response.json(user);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
